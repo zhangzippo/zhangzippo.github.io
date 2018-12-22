b7c7788c7889fe17245991c2192ef2d181aeb6d2
zippo <zhangzhp3@163.com>
2018-12-22 18:42:03 +0800

    save

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,15 +1,17 @@
 module.exports = {
+    // dest: 'zhangZippo.github.io/.vuepress/dist',
+    // base:'/',
     title: 'zippo‘s blog',
     description:'前端程序员',
     themeConfig: {
       nav: [
         { text: 'Home', link: '/' },
         { text: 'Archives', link: '/Archives/' },
-        { text: 'docs', link: '/docs/' },
+        { text: 'article', link: '/article/' },
         { text: 'github', link: 'https://github.com/zhangZippo' },
       ],
       sidebar: {
-        '/docs/':[
+        '/article/':[
           '',
           ['page-a','adasdadasd'],
           ['page-b', 'Explicit link text']
---
+++ .vuepress/dist/assets/js/5.69a63c1d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{170:function(t,n,e){"use strict";e.r(n);var s=e(0),a=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);a.options.__file="page-a.md";n.default=a.exports}}]);
